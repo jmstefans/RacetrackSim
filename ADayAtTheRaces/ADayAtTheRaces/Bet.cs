7cd49cff6c0d712359c813027ff0c698f777f02b
Rommel Rico <rommeltj@gmail.com>
2013-08-18 22:02:49 -0700

    Added Bet class skeleton.

--- ADayAtTheRaces/ADayAtTheRaces/Bet.cs
+++ ADayAtTheRaces/ADayAtTheRaces/Bet.cs
@@ -8,5 +8,26 @@ namespace ADayAtTheRaces
 {
     class Bet
     {
+        public int Amount;
+        public int Dog;
+        public Guy Bettor;
+
+        public string GetDescription()
+        {
+            /* TODO:
+             * 1. Initialize string.
+             * 2. Add Bettor's name, how much cash was bet, and which dog was bet on to string.
+             * 2a. Ex: "Joe bets $8 on dog #4."
+             * 2b. If amount is 0, "Joe hasn't placed a bet."
+             */
+        }
+
+        public int PayOut(int Winner)
+        {
+            /* TODO:
+             * 1. If the dog won, return the amount bet. 
+             *    Else return the negative of the amount bet.
+             */
+        }
     }
 }
