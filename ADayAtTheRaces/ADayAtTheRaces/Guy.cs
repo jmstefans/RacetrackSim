56f8c130697d593ba90ca58390cd3de6f8f023d1
Rommel Rico <rommeltj@gmail.com>
2013-08-18 21:29:20 -0700

    Added Guy class skeleton.

--- ADayAtTheRaces/ADayAtTheRaces/Guy.cs
+++ ADayAtTheRaces/ADayAtTheRaces/Guy.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ADayAtTheRaces
 {
     class Guy
     {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+        public RadioButton MyRadioButton;
+        public Label MyLabel;
+
+        public void UpdateLabels()
+        {
+            /* TODO:
+             * 1. Set MyLabel to my bet's description.
+             * 2. Set label on MyRadioButton to show my cash.
+             */
+        }
+
+        public void ClearBet()
+        {
+            /* TODO:
+             * 1. Reset bet to 0.
+             */
+        }
+
+        public bool PlaceBet(int Amount, int Dog)
+        {
+            /* TODO:
+             * 1. Place new bet.
+             * 2. Store in MyBet field.
+             * 3. Return true if Guy had enough money to bet.
+             */
+        }
+
+        public void Collect(int Winner)
+        {
+            /* TODO:
+             * 1. Call Bet object to PayOut.
+             * 2. Be gangsta rich.
+             */
+        }
     }
 }
