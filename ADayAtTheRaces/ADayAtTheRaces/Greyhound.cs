03b75104fe84cc141c3c8c96a688d6e0678439e9
Rommel Rico <rommeltj@gmail.com>
2013-08-18 21:20:32 -0700

    Added Greyhound skeleton and initial commit.

---
+++ ADayAtTheRaces/ADayAtTheRaces/Greyhound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADayAtTheRaces
+{
+    class Greyhound
+    {
+        public int StartingPosition;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random Randomizer;
+
+        public bool Run()
+        {
+            /*TODO:
+             * 1. Move forward a random space between 1 and 4.
+             * 2. Update the position of MyPictureBox on the form.
+             * 3. Return true if Greyhound wins the race.
+            */
+        }
+
+        public void TakeStartingPosition()
+        {
+            /*TODO:
+             * 1. Reset location to start line.
+            */
+        }
+    }
+}
