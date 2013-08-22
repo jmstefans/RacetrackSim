f0d69905b873a4f31a2860a7230a2e4211f1c5d7
jmstefans <jstefanski@sandiego.edu>
2013-08-21 20:20:53 -0700

    Added public to the class declaration.

--- ADayAtTheRaces/ADayAtTheRaces/Greyhound.cs
+++ ADayAtTheRaces/ADayAtTheRaces/Greyhound.cs
@@ -8,7 +8,7 @@ using System.Drawing;
 
 namespace ADayAtTheRaces
 {
-    class Greyhound
+    public class Greyhound
     {
         public int StartingPosition;
         public int RacetrackLength;
